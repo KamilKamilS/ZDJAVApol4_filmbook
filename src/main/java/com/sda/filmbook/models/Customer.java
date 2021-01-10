2e927da0fea3bf5e26590a813e27d5db32ef1438
Kamil <s.kamilx@gmail.com>
2021-01-10 12:20:54 +0100

    Create Customer class

---
+++ src/main/java/com/sda/filmbook/models/Customer.java
@@ -0,0 +1,11 @@
+package com.sda.filmbook.models;
+
+public class Customer {
+
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private String email;
+    private String adress;
+    private List<Order> orders;
+}
