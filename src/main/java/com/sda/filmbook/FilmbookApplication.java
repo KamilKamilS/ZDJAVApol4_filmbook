bcf6798e4af08c38ce7d190802475d4d076a08a7
Kamil <s.kamilx@gmail.com>
2021-01-10 12:01:30 +0100

    Init project

---
+++ src/main/java/com/sda/filmbook/FilmbookApplication.java
@@ -0,0 +1,13 @@
+package com.sda.filmbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmbookApplication.class, args);
+    }
+
+}
