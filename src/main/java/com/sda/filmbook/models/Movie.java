2402e32a8164a1ce4980cadb0a5ad694adcf5497
Kamil <s.kamilx@gmail.com>
2021-01-10 12:22:59 +0100

    Create Movie class

---
+++ src/main/java/com/sda/filmbook/models/Movie.java
@@ -0,0 +1,10 @@
+package com.sda.filmbook.models;
+
+public class Movie {
+
+    private String title;
+    private String description;
+    private Genre genre;
+    private int yearOfProduction;
+    private List<Rate> rates;
+}
