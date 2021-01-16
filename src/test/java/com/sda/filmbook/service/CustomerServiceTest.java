a774b25dbcc5a7de5ef55296581d69b3dba267c6
Kamil <s.kamilx@gmail.com>
2021-01-16 12:48:46 +0100

    Add CustomerServiceTest

---
+++ src/test/java/com/sda/filmbook/service/CustomerServiceTest.java
@@ -0,0 +1,18 @@
+package com.sda.filmbook.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CustomerServiceTest {
+
+    public void shouldCreateNewCustomer() {
+        // given
+
+
+        // when
+
+        // then
+    }
+}
