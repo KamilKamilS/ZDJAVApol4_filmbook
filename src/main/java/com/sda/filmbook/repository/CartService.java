472876b0fc4d85d2aa2b52645ccbc84160202f00
Kamil <s.kamilx@gmail.com>
2021-01-17 10:28:54 +0100

    Refactor SessionCartService

--- src/main/java/com/sda/filmbook/repository/CartService.java
+++ src/main/java/com/sda/filmbook/repository/CartService.java
@@ -4,9 +4,11 @@ import com.sda.filmbook.model.Movie;
 import com.sda.filmbook.service.SessionCart;
 import com.sda.filmbook.service.exception.MovieNotFoundInCatalogueException;
 
+import java.util.Map;
+
 public interface CartService {
 
     void addMovieToCart(Movie movie) throws MovieNotFoundInCatalogueException;
 
-    SessionCart getSessionCart();
+    Map<Movie, Integer> getSessionCart();
 }
