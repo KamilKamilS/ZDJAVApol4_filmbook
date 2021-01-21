501a520471a12923262e2c8a6069cca3322c965a
Kamil <s.kamilx@gmail.com>
2021-01-21 22:34:29 +0100

    Add getAllPremiere method to MovieController

--- src/main/java/com/sda/filmbook/controller/MovieController.java
+++ src/main/java/com/sda/filmbook/controller/MovieController.java
@@ -1,8 +1,22 @@
 package com.sda.filmbook.controller;
 
+import com.sda.filmbook.model.Movie;
+import com.sda.filmbook.service.MovieService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@AllArgsConstructor
 @RestController
 public class MovieController {
+
+    MovieService movieService;
+
+    @GetMapping("/movie/premiere")
+    public List<Movie> getAllPremiere() {
+       return movieService.getPremiereMovies();
+    }
 
 }
