f0934ab123cacb452fe98f933a53a34ef902b1c7
Kamil <s.kamilx@gmail.com>
2021-01-22 12:12:32 +0100

    Update test for Services

--- src/test/java/com/sda/filmbook/service/RateServiceMockedTest.java
+++ src/test/java/com/sda/filmbook/service/RateServiceMockedTest.java
@@ -1,15 +1,18 @@
-/*
 package com.sda.filmbook.service;
 
+import com.sda.filmbook.model.Genre;
 import com.sda.filmbook.model.Movie;
 import com.sda.filmbook.model.Rate;
 import com.sda.filmbook.repository.MovieRepository;
 import com.sda.filmbook.service.exception.MovieNotFoundInCatalogueException;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Arrays;
@@ -20,25 +23,29 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@RunWith(JUnitPlatform.class)
+//@RunWith(JUnitPlatform.class)
+@SpringBootTest
 class RateServiceMockedTest {
 
     @InjectMocks
+    @Autowired
     RateService rateService;
 
     @MockBean
     MovieRepository movieRepository;
 
-    @Before
-    public void init() {
-        MockitoAnnotations.initMocks(this);
-    }
+//    @Before
+//    public void init() {
+//        MockitoAnnotations.initMocks(this);
+//    }
 
     @Test
     public void shouldAddNewRateToGivenMovie() throws MovieNotFoundInCatalogueException {
         // given
         Movie movie = new Movie();
         movie.setTitle("Test title");
+        movie.setGenre(Genre.DRAMA);
+        movie.setDescription("aaa");
 
         Rate rate = new Rate();
         rate.setRate(7);
@@ -49,9 +56,10 @@ class RateServiceMockedTest {
 
         // when
         List<Rate> rateList = rateService.addNewRate(movie, rate);
+        rateList = rateService.addNewRate(movie, rate);
 
         // then
-        assertThat(rateList).isEqualTo(Arrays.asList(rate));
+        assertThat(rateList.size()).isEqualTo(2);
     }
 
-}*/
+}
--- src/test/java/com/sda/filmbook/service/RateServiceTest.java
+++ src/test/java/com/sda/filmbook/service/RateServiceTest.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.annotation.DirtiesContext.*;
 
@@ -28,12 +30,14 @@ public class RateServiceTest {
     @Autowired
     MovieRepository movieRepository;
 
+
     @Test
     public void shouldAddNewMovieWithRatesToCatalogue() throws Throwable {
         // given
         Movie testMovie = new Movie();
         testMovie.setTitle("Ogniem i Mieczem");
         testMovie.setGenre(Genre.DRAMA);
+        testMovie.setDescription("description");
 
         Rate rate = new Rate();
         rate.setRate(7);
@@ -62,15 +66,27 @@ public class RateServiceTest {
         Movie movieFromCatalogue2 = movieService.readMovieFromCatalogue(testMovie.getTitle());
 
         assertThat(movieFromCatalogue2.getRates().size()).isEqualTo(2);
+
+        Rate rate3 = new Rate();
+        rate2.setRate(8);
+        rate2.setDescription("Test description");
+        rate2.setMovie(testMovie);
+
+        rateService.addNewRate(movieFromCatalogue, rate2);
+
+        Movie movieFromCatalogue3 = movieService.readMovieFromCatalogue(testMovie.getTitle());
+
+        assertThat(movieFromCatalogue3.getRates().size()).isEqualTo(3);
     }
 
-    @Disabled
+
     @Test
     public void shouldAddNewRatesToMovieInCatalogue() throws Throwable{
         // given
         Movie testMovie = new Movie();
         testMovie.setTitle("Ogniem i Mieczem");
         testMovie.setGenre(Genre.DRAMA);
+        testMovie.setDescription("description");
 
         Rate rate = new Rate();
         rate.setRate(7);
@@ -79,14 +95,17 @@ public class RateServiceTest {
 
         Rate rate2 = new Rate();
         rate.setRate(3);
-        rate.setDescription("Test description");
+        rate.setDescription("Test descriptionn");
         rate.setMovie(testMovie);
 
         movieService.addMovieToCatalogue(testMovie);
         Movie movieFromCatalogue = movieService.readMovieFromCatalogue(testMovie.getTitle());
         // when
 
-        rateService.addNewRate(movieFromCatalogue, rate);
+        List<Rate> rateList = rateService.addNewRate(movieFromCatalogue, rate);
+        assertThat(rateList.size()).isEqualTo(1);
+
+        Movie movieFromCatalogue1 = movieService.readMovieFromCatalogue(testMovie.getTitle());
         rateService.addNewRate(movieFromCatalogue, rate2);
 
         Movie movieFromCatalogue2 = movieService.readMovieFromCatalogue(testMovie.getTitle());
