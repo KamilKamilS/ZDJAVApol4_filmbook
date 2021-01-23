8552d5751ca4129a5b774ea11b57e6a0029a9562
Kamil <s.kamilx@gmail.com>
2021-01-23 01:53:24 +0100

    Add DirtiesContext annotation to MovieTestControllerTest

--- src/test/java/com/sda/filmbook/controller/MovieTestControllerTest.java
+++ src/test/java/com/sda/filmbook/controller/MovieTestControllerTest.java
@@ -7,13 +7,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import javax.validation.ConstraintViolationException;
 import java.time.LocalDate;
 import java.util.List;
 
-
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest
 class MovieTestControllerTest {
 
