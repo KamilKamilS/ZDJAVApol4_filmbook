4c8c9a291caa1f4968b74afdc61ee3a926da6627
Kamil <s.kamilx@gmail.com>
2021-01-23 10:08:32 +0100

    Create RateController with addNewRateToMovie method

---
+++ src/main/java/com/sda/filmbook/controller/RateController.java
@@ -0,0 +1,25 @@
+package com.sda.filmbook.controller;
+
+import com.sda.filmbook.model.Rate;
+import com.sda.filmbook.service.RateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RateController {
+
+    private RateService rateService;
+
+    @PostMapping("/rate")
+    public ResponseEntity<Rate> addNewRateToMovie(@RequestBody Rate rate,
+                                                  @RequestParam(value = "movieId", required = true) Long movieId) {
+        return ResponseEntity.status(HttpStatus.OK).body(rateService.addNewRate(movieId, rate));
+    }
+
+}
